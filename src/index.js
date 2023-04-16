678d3ba619eb0d84cf4be052191b4e63ceacf40a
Tong Tang <pichip22@hotmail.com>
2023-04-16 00:15:17 -0400

    completed?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const description = document.getElementById(`new-task-description`);
+  const form = document.getElementById(`create-task-form`);
+  const tasks = document.getElementById(`tasks`);
+  const priority = document.getElementById(`priority`);
+
+  form.addEventListener(`submit`,function(e){
+    e.preventDefault();
+    
+    const taskList = document.createElement(`li`);
+    taskList.innerText = priority.value+ `. ` + description.value;
+    
+    const remove_btn = document.createElement(`button`);
+    remove_btn.innerText = `x`;
+
+    remove_btn.addEventListener(`click`,function(e){
+      e.target.parentNode.remove(taskList);
+    });
+    taskList.appendChild(remove_btn);
+    tasks.appendChild(taskList);
+    priority.value =``;
+    description.value=``;
+  });
 });
